a531ad5978d3d6bb3fe9935a21b28a273ad9d201
Jahangir Anwari <jahangir@tarkalabs.com>
2019-06-19 18:31:08 +0530

    Add custom meta to WooCommerce order payload

--- wp-custom-filters.php
+++ wp-custom-filters.php
@@ -8,3 +8,15 @@ add_filter( 'http_request_args', function( $args ) {
   $args['reject_unsafe_urls'] = false;
   return $args;
 });
+
+/**
+*
+* Add custom meta to WooCommerce order payload
+*/
+add_action( 'woocommerce_new_order_item', 'tp_add_custom_meta_to_order', 99, 3);
+function tp_add_custom_meta_to_order( $item_id, $item, $order_id ) {
+  wc_add_order_item_meta($item_id, 'Programs', $item->get_name() );
+  wc_add_order_item_meta($item_id, 'firstname', 'AA' );
+  wc_add_order_item_meta($item_id, 'lastname', 'BB' );
+  wc_add_order_item_meta($item_id, 'email', 'e@ma.il' );
+}
